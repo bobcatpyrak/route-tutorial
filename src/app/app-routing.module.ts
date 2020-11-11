81cc5b678661567dfaa627bd4eb5eaa6e8ee2444
Jonathan Pyrak <bobcatpyrak@gmail.com>
2020-11-11 14:50:31 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { HelpComponent } from "./help/help.component";
+import { E404Component } from "./e404/e404.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent},
+  { path: "about", component: AboutComponent},
+  { path: "help", component: HelpComponent},
+  { path: "**", component: E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
+import { E404Component } from './e404/e404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HelpComponent,
+    E404Component
   ],
   imports: [
     BrowserModule,
